664207e5dd4e9e44a7af0a203164e91cf504421c
andy-06 <mail@andychen.de>
2020-11-25 23:52:59 +0100

    delete credentials as it does not work

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ load_dotenv()
 reddit = praw.Reddit(
     client_id=os.getenv('REDDIT_CLIENT_ID'),
     client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-    user_agent='Comment extraction',
-    username=os.getenv('REDDIT_USERNAME'),
-    password=os.getenv('REDDIT_PASSWORD')
+    user_agent='Comment extraction'
 )
-
-print(reddit.user.me())
 
 url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
 submission = reddit.submission(url=url)
