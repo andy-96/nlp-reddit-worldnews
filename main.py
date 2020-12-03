4da052c511321968b436f28a7425c490abfae0bc
andy-06 <mail@andychen.de>
2020-12-03 16:24:36 +0100

    create dataset function

--- main.py
+++ main.py
@@ -1,29 +1,65 @@
 import praw
+from praw.models import MoreComments
 import os
 from dotenv import load_dotenv
-
+import pickle
 load_dotenv()
 
-reddit = praw.Reddit(
-    client_id=os.getenv('REDDIT_CLIENT_ID'),
-    client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-    user_agent='Comment extraction'
-)
+class RedditCrawler:
+    def __init__(self):
+        self.reddit = praw.Reddit(
+            client_id=os.getenv('REDDIT_CLIENT_ID'),
+            client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+            user_agent='Comment extraction'
+        )
 
+    def fetchAndSaveTopPostsOfToday(self):
+        top_subreddits = []
+        top_comments = []
 
-subreddit = reddit.subreddit('worldnews')
-posts = subreddit.top('day', limit=1000)
-for post in posts:
-    print(post.title)
-    print(post.score)
-    print(post.num_comments)
-    print(post.selftext)
-    print(post.id)
-    print(post.url)
-    permalink = post.permalink
-    submission = reddit.submission(url=f'https://www.reddit.com{permalink}')
-    submission.comment_sort = 'best'
-    submission.comment_limit = 5
-    for top_level_comment in submission.comments:
-        print(top_level_comment.body)
-    break
+        subreddit = self.reddit.subreddit('worldnews')
+        posts = subreddit.top('month', limit=1000)
+        for i, post in enumerate(posts):
+            postData = {
+                'title': post.title,
+                'score': post.score,
+                'num_comments': post.num_comments,
+                'body': post.selftext,
+                'id': post.id,
+                'url': post.url,
+                'permalink': post.permalink,
+                'created_utc': post.created_utc
+            }
+            top_subreddits.append(postData)
+
+            submission = self.reddit.submission(url=f'https://www.reddit.com{postData["permalink"]}')
+            submission.comment_sort = 'best'
+            submission.comment_limit = 100
+            for comment in submission.comments:
+                # check if end of page
+                if isinstance(comment, MoreComments):
+                    continue
+                commentData = {
+                    'id': comment.id,
+                    'body': comment.body,
+                    'author': comment.author,
+                    'score': comment.score,
+                    'created_utc': comment.created_utc,
+                    'postId': postData['id']
+                }
+                top_comments.append(commentData)
+
+            if i % 10 == 0:
+                print(f'Crawled {i}/1000')
+
+        with open('top_subreddits.pkl', 'wb') as f:
+            pickle.dump(top_subreddits, f)
+            f.close()
+
+        with open('top_comments.pkl', 'wb') as f:
+            pickle.dump(top_comments, f)
+            f.close()
+
+if __name__ == "__main__":
+    redditCrawler = RedditCrawler()
+    redditCrawler.fetchAndSaveTopPostsOfToday()
