ba1218e9b9498daf1cf928ac1d93301bfc91ed5c
andy-06 <mail@andychen.de>
2020-12-03 12:09:02 +0100

    create skeleton call for reddit worldnews

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ reddit = praw.Reddit(
     user_agent='Comment extraction'
 )
 
-print("Ma")
-url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
-submission = reddit.submission(url=url)
-for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+
+subreddit = reddit.subreddit('worldnews')
+posts = subreddit.top('day', limit=1000)
+for post in posts:
+    print(post.title)
+    print(post.score)
+    print(post.num_comments)
+    print(post.selftext)
+    print(post.id)
+    print(post.url)
+    permalink = post.permalink
+    submission = reddit.submission(url=f'https://www.reddit.com{permalink}')
+    submission.comment_sort = 'best'
+    submission.comment_limit = 5
+    for top_level_comment in submission.comments:
+        print(top_level_comment.body)
+    break
