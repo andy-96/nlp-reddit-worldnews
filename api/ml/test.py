348e38c525dfa761087bef503420b06653e1be83
andy-06 <mail@andychen.de>
2021-01-20 09:02:17 +0100

    add test file

--- api/ml/test.py
+++ api/ml/test.py
@@ -16,9 +16,40 @@ def get_tokenized_headline():
     return headline_tokenizer, comment_tokenizer
 
 
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    
+    # add extra dimensions to add the padding
+    # to the attention logits. Tensor sizes are always a pain...
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+
+def create_masks(input, target):
+    # Encoder padding mask
+    encoder_padding_mask = create_padding_mask(input)
+    
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    decoder_padding_mask = create_padding_mask(input)
+    
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by 
+    # the decoder.
+    look_ahead_mask = create_look_ahead_mask(tf.shape(target)[1])
+    decoder_target_padding_mask = create_padding_mask(target)
+    combined_mask = tf.maximum(decoder_target_padding_mask, look_ahead_mask)
+    
+    return encoder_padding_mask, decoder_padding_mask, combined_mask
+
+
 def translate(sentence):
     preprocessing = Preprocessing()
-    headline_tokenizer = get_tokenized_headline()
+    headline_tokenizer, comment_tokenizer = get_tokenized_headline()
 
     sentence = preprocessing.preprocess_sentence(sentence)
     input = headline_tokenizer.texts_to_sequences([sentence])
